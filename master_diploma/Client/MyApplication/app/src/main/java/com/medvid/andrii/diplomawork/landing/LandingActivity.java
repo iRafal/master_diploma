2fff02c849d002ec0a6acc9722ed9d953f39c99b
DESKTOP-MI4726M\andri <Andrii.Medvid@symphony-solutions.eu>
2016-11-26 19:30:52 +0200

    Configured .gradle build file. Created layouts for registeation/login/landing screens. Created registration/landing/ login activitiees.

---
+++ master_diploma/Client/MyApplication/app/src/main/java/com/medvid/andrii/diplomawork/landing/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.medvid.andrii.diplomawork.landing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.medvid.andrii.diplomawork.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
